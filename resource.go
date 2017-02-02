0b4da72dbc7577a89d216ea4fbbbf3d10715a6b5
Hugo Wetterberg <hugo@wetterberg.nu>
2017-02-02 11:08:41 +0100

    initial commit

---
+++ resource.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Sydsvenskan/concourse"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	context, err := concourse.NewContext(os.Args, os.Stdin, os.Stdout, os.Stderr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to create command context:", err.Error())
+		os.Exit(1)
+		return
+	}
+
+	context.Handle(&concourse.Resource{
+		Check: &CheckCommand{},
+		In:    &InCommand{},
+		Out:   &OutCommand{},
+	})
+}
+
+// CheckCommand check-command payload
+type CheckCommand struct {
+	// Source definition
+	Source Source `json:"source"`
+	// Version information passed to the resource
+	Version concourse.ResourceVersion `json:"version"`
+}
+
+// HandleCommand runs the command
+func (cmd *CheckCommand) HandleCommand(ctx *concourse.CommandContext) (
+	*concourse.CommandResponse, error,
+) {
+	var err error
+	resp := concourse.CommandResponse{
+		Versions: make([]concourse.ResourceVersion, 0, 0),
+	}
+
+	if cmd.Version != nil {
+		resp.Versions = append(resp.Versions, cmd.Version)
+	}
+
+	etag := cmd.Version["etag"]
+	hash := cmd.Version["sha1"]
+
+	timeout := 5 * time.Minute
+	if cmd.Source.Timeout != "" {
+		timeout, err = time.ParseDuration(cmd.Source.Timeout)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse timeout")
+		}
+	}
+
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("GET", cmd.Source.URL, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create request")
+	}
+
+	// Add source headers
+	for name, values := range cmd.Source.Headers {
+		req.Header[name] = append(req.Header[name], values...)
+	}
+
+	if etag != "" {
+		req.Header.Add("If-None-Match", etag)
+	}
+
+	if cmd.Source.BasicAuth != nil {
+		req.SetBasicAuth(
+			cmd.Source.BasicAuth.User,
+			cmd.Source.BasicAuth.Password,
+		)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to perform request")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusNotModified {
+		return &resp, nil
+	}
+
+	version := concourse.ResourceVersion{}
+	responseETag := res.Header.Get("ETag")
+	if responseETag != "" {
+		// Catch cases where an identical Etag is returned but the
+		// server responded with a 200 OK anyway.
+		if responseETag == etag {
+			return &resp, nil
+		}
+
+		version["etag"] = responseETag
+
+	} else {
+		h := sha1.New()
+		_, err := io.Copy(h, res.Body)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to hash response contents")
+		}
+		version["sha1"] = fmt.Sprintf("%x", h.Sum(nil))
+
+		if version["sha1"] == hash {
+			return &resp, nil
+		}
+	}
+
+	// Prepend the new version to the versions array
+	resp.Versions = append([]concourse.ResourceVersion{
+		version,
+	}, resp.Versions...)
+
+	return &resp, nil
+}
+
+type Source struct {
+	URL       string      `json:"url"`
+	Timeout   string      `json:"timeout"`
+	Headers   http.Header `json:"headers,omitempty"`
+	BasicAuth *BasicAuth  `json:"basic_auth,omitempty"`
+}
+
+type BasicAuth struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+// InCommand in-command payload
+type InCommand struct {
+	// Source definition
+	Source Source `json:"source"`
+	// Version is used in the implicit post `put` `get`
+	Version concourse.ResourceVersion
+}
+
+// HandleCommand runs the command
+func (cmd *InCommand) HandleCommand(ctx *concourse.CommandContext) (
+	*concourse.CommandResponse, error,
+) {
+	var err error
+	var resp concourse.CommandResponse
+
+	etag := cmd.Version["etag"]
+	hash := cmd.Version["sha1"]
+
+	timeout := 5 * time.Minute
+	if cmd.Source.Timeout != "" {
+		timeout, err = time.ParseDuration(cmd.Source.Timeout)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse timeout")
+		}
+	}
+
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("GET", cmd.Source.URL, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create request")
+	}
+
+	// Add source headers
+	for name, values := range cmd.Source.Headers {
+		req.Header[name] = append(req.Header[name], values...)
+	}
+
+	if cmd.Source.BasicAuth != nil {
+		req.SetBasicAuth(
+			cmd.Source.BasicAuth.User,
+			cmd.Source.BasicAuth.Password,
+		)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to perform request")
+	}
+	defer res.Body.Close()
+
+	version := concourse.ResourceVersion{}
+	responseETag := res.Header.Get("ETag")
+	if etag != "" && etag != responseETag {
+		return nil, errors.Errorf(
+			"unexpected ETag %q, expected %q", responseETag, etag,
+		)
+	}
+	if responseETag != "" {
+		version["etag"] = responseETag
+	}
+	output, err := os.Create(filepath.Join(ctx.Directory(), "downloaded"))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create file for the download")
+	}
+	tee := io.TeeReader(res.Body, output)
+
+	h := sha1.New()
+	_, err = io.Copy(h, tee)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to write out download")
+	}
+	version["sha1"] = fmt.Sprintf("%x", h.Sum(nil))
+
+	if hash != "" && version["sha1"] != hash {
+		return nil, errors.Errorf("unexpected SHA1 content hash %q, expected %q",
+			version["sha1"], hash,
+		)
+	}
+
+	resp.Version = version
+	resp.AddMeta("content-type", res.Header.Get("Content-type"))
+
+	return &resp, nil
+}
+
+// OutCommand in-command payload
+type OutCommand struct{}
+
+// HandleCommand runs the command
+func (cmd *OutCommand) HandleCommand(ctx *concourse.CommandContext) (
+	*concourse.CommandResponse, error,
+) {
+	return nil, errors.New("not implemented")
+}
---
+++ vendor/github.com/Sydsvenskan/concourse/context.go
@@ -0,0 +1,198 @@
+package concourse
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// CommandContext is passed to the in, out, and check commands.
+type CommandContext struct {
+	directory   string
+	commandName string
+	in          io.Reader
+	out         io.Writer
+	Log         io.Writer
+}
+
+// Resource is the default resource implementation
+type Resource struct {
+	Check CommandHandler
+	In    CommandHandler
+	Out   CommandHandler
+}
+
+// CheckHandler returns the registered check handler
+func (r *Resource) CheckHandler() CommandHandler {
+	return r.Check
+}
+
+// InHandler returns the registered get handler
+func (r *Resource) InHandler() CommandHandler {
+	return r.In
+}
+
+// OutHandler returns the registered put handler
+func (r *Resource) OutHandler() CommandHandler {
+	return r.Out
+}
+
+// ResourceHandler provides handler implementations
+type ResourceHandler interface {
+	CheckHandler() CommandHandler
+	InHandler() CommandHandler
+	OutHandler() CommandHandler
+}
+
+// CommandHandler handles a /opt/resource/{in,out,check} concourse command
+type CommandHandler interface {
+	HandleCommand(ctx *CommandContext) (*CommandResponse, error)
+}
+
+// ResourceVersion is arbitrary version info that identifies ar
+type ResourceVersion map[string]string
+
+// CommandResponse is what get's returned to Concourse (JSON on Stdout) when
+// we've successfully completed the task.
+type CommandResponse struct {
+	Version  ResourceVersion           `json:"version"`
+	Versions []ResourceVersion         `json:"-"`
+	Metadata []CommandResponseMetadata `json:"metadata"`
+}
+
+// VersionsResponse is a utility function to create a check response
+func VersionsResponse(version ...ResourceVersion) []ResourceVersion {
+	return version
+}
+
+// AddMeta is a helper function for appending new metadata entries
+func (cr *CommandResponse) AddMeta(name, value string) {
+	cr.Metadata = append(cr.Metadata, CommandResponseMetadata{
+		Name:  name,
+		Value: value,
+	})
+}
+
+// CommandResponseMetadata is a metadata entry in our CommandResponse.
+type CommandResponseMetadata struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// NewContext creates a new context
+func NewContext(
+	args []string, in io.Reader, out io.Writer, log io.Writer,
+) (*CommandContext, error) {
+	ctx := &CommandContext{
+		in:  in,
+		out: out,
+		Log: log,
+	}
+
+	ctx.commandName = filepath.Base(args[0])
+	if len(args) > 1 {
+		ctx.directory = args[1]
+	}
+
+	return ctx, nil
+}
+
+// Handle the command
+func (ctx *CommandContext) Handle(handler ResourceHandler) {
+	var cmdHandler CommandHandler
+	decoder := json.NewDecoder(ctx.in)
+
+	switch ctx.commandName {
+	case "out":
+		cmdHandler = handler.OutHandler()
+		if handler == nil {
+			fmt.Fprintf(ctx.Log,
+				"the command %q is not implemented", ctx.commandName,
+			)
+			_, _ = ctx.out.Write([]byte("{}"))
+			return
+		}
+	case "in":
+		cmdHandler = handler.InHandler()
+		if handler == nil {
+			fmt.Fprintf(ctx.Log,
+				"the command %q is not implemented", ctx.commandName,
+			)
+			_, _ = ctx.out.Write([]byte("{}"))
+			return
+		}
+	case "check":
+		cmdHandler = handler.CheckHandler()
+		if handler == nil {
+			fmt.Fprintf(ctx.Log,
+				"the command %q is not implemented", ctx.commandName,
+			)
+			_, _ = ctx.out.Write([]byte("[]"))
+			return
+		}
+	default:
+		fmt.Fprintf(ctx.Log, "unknown command: %q", ctx.commandName)
+		os.Exit(1)
+	}
+
+	// Decode the input as the selected command
+	if err := decoder.Decode(cmdHandler); err != nil {
+		fmt.Fprintln(ctx.Log, "failed to decode input json", err.Error())
+	}
+
+	// Change directory if specified
+	if ctx.directory != "" {
+		if err := os.Chdir(ctx.directory); err != nil {
+			fmt.Fprintf(ctx.Log, "failed to change directory to %q\n", ctx.directory)
+		}
+	}
+
+	// Run the command handler
+	res, err := cmdHandler.HandleCommand(ctx)
+	if err != nil {
+		fmt.Fprintln(ctx.Log, "failed to run command", err.Error())
+		os.Exit(1)
+	}
+
+	// Encode our output, with some special-casing for check
+	encoder := json.NewEncoder(ctx.out)
+	if ctx.commandName == "check" {
+		err = encoder.Encode(res.Versions)
+	} else {
+		err = encoder.Encode(res)
+	}
+	if err != nil {
+		fmt.Fprintln(ctx.Log, "failed to encode response", err.Error())
+		os.Exit(1)
+	}
+}
+
+// JSON encodes and writes out a JSON result in the output directory.
+func (ctx *CommandContext) JSON(path string, obj interface{}) error {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return errors.Wrapf(err,
+			"failed to marshal JSON for writing to %s/%s", ctx.directory, path)
+	}
+	return ctx.File(path, data)
+}
+
+// File writes out a file in the output directory.
+func (ctx *CommandContext) File(name string, data []byte) error {
+	fullPath := path.Join(ctx.directory, name)
+	return errors.Wrapf(
+		ioutil.WriteFile(fullPath, data, 0666),
+		"failed to write data to %s", fullPath,
+	)
+}
+
+// Directory returns the resource directory.
+func (ctx *CommandContext) Directory() string {
+	return ctx.directory
+}
